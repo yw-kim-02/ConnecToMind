cbdf1a021ce78809f7e29162feda9b05e3ac9194
gunwoof <gunwoof1234@naver.com>
2025-03-26 05:13:38 +0000

    Release-0

--- 5-mindeye_code/mindeye1/models.py
+++ 5-mindeye_code/mindeye1/models.py
@@ -75,6 +75,11 @@ class Clipper(torch.nn.Module):
         self.clip = clip_model
 
     def embed_image(self, image):
+        '''
+        clip_emb(hidden_state 없을 때): 최종 image embedding - shape: [batch, embedding_dim]
+        clip_emb(hidden_state 있을 때): 최종 image embedding - shape: [batch, sequence_length, embedding_dim]
+            Tip: sequence_length는 vit의 sequence의 patch 개수
+        '''
         if self.hidden_state:
             clip_emb = self.preprocess((image).to(self.device)) # image preprocess ex) image값 -1 ~ 1
             clip_emb = self.image_encoder(clip_emb)
@@ -96,6 +101,10 @@ class Clipper(torch.nn.Module):
         return clip_emb
     
     def _versatile_normalize_embeddings(self, encoder_output):
+        '''
+        embeds(hidden_state 없을 때): image embedding normalization - shape: [batch, sequence_length, embedding_dim]
+            Tip: sequence_length는 vit의 sequence의 patch 개수
+        '''
         embeds = encoder_output.last_hidden_state
         embeds = self.image_encoder.vision_model.post_layernorm(embeds)
         embeds = self.image_encoder.visual_projection(embeds)
@@ -190,11 +199,9 @@ class BrainNetwork(nn.Module):
         
     def forward(self, x):
         '''
-            bs, 1, 15724 -> bs, 32, h
-            bs, 32, h -> bs, 32h
-            b2, 32h -> bs, 768
+        x(MLP backbone): fmri -> mlp - shape: [batch, 768]
+        x(MLP projector): fmri -> mlp -> mlp - shape: ([batch, 768], [batch, 257, 768])
         '''
-
         # fMRI volume 그대로 들어올 때
         if x.ndim == 4:
             # assert x.shape[1] == 81 and x.shape[2] == 104 and x.shape[3] == 83, "fMRI data shape 안 맞음" # [N, 81, 104, 83]
@@ -215,6 +222,57 @@ class BrainNetwork(nn.Module):
         if self.use_projector:
             return x, self.projector(x.reshape(len(x), -1, self.clip_size))
         return x
+
+class BrainDiffusionPrior(DiffusionPrior):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, text_embed = None, image_embed = None, *args, **kwargs):
+        '''
+        loss: loss(prediction x_0, target x_0) - shape: Scala
+        pred: x_t -> x_t-1 denoise한 결과 - shape: [batch_size, embedding_dim]
+        '''
+        batch, device = image_embed.shape[0], image_embed.device
+        times = self.noise_scheduler.sample_random_times(batch)
+
+        image_embed *= self.image_embed_scale
+
+        # calculate forward loss
+        loss, pred = self.p_losses(image_embed=image_embed, times=times, text_embed=text_embed, *args, **kwargs)
+        
+        # undo the scaling so we can directly use it for real mse loss and reconstruction
+        return loss, pred
+
+    def p_losses(self, image_embed, times, text_embed=None, noise = None):
+        '''
+        loss: loss(prediction x_0, target x_0) - shape: Scala
+        pred: x_t -> x_t-1 denoise한 결과 - shape: [batch_size, embedding_dim]
+        '''
+        # noise 정의
+        noise = default(noise, lambda: torch.randn_like(image_embed)) 
+
+        # random한 t가 들어옴 -> x_0에서 한 번에 t까지의 noise를 씌움 
+        image_embed_noisy = self.noise_scheduler.q_sample(x_start = image_embed, t = times, noise = noise)
+
+        # self conditioning: prediction 값(x_0) = function(x_t, image_embed_noisy(첫 번째 prediction값)) 
+        self_cond = None
+        if self.net.self_cond and random.random() < 0.5:
+            with torch.no_grad():
+                # self.net은 x_0의 prediction 값을 반환
+                self_cond = self.net(image_embed_noisy, times, text_embed=text_embed).detach() # 첫 번째 prediction값을 뽑기위해 self.net사용
+
+        # prediction 값(x_0)
+        pred = self.net(image_embed_noisy, times, self_cond = self_cond, text_embed=text_embed, text_cond_drop_prob = self.text_cond_drop_prob, image_cond_drop_prob = self.image_cond_drop_prob) # cond_drop_prob: 텍스트 & 이미지 드롭아웃 확률 
+
+        # prediction 값(x_0) normalization
+        if self.predict_x_start and self.training_clamp_l2norm:
+            pred = self.l2norm_clamp_embed(pred)
+
+        # x_0를 비교하는 방식
+        target = image_embed
+
+        loss = self.noise_scheduler.loss_fn(pred, target) # diffusion prior에서는 ε가 아닌 x_0를 예측함
+        return loss, pred
 
 class BrainDiffusionPriorOld(DiffusionPrior):
     """ 
@@ -312,6 +370,7 @@ class BrainDiffusionPriorOld(DiffusionPrior):
         *args,
         **kwargs
     ):
+        # 
         assert exists(text) ^ exists(text_embed) ^ exists(voxel), 'either text, text embedding, or voxel must be supplied'
         assert exists(image) ^ exists(image_embed), 'either image or image embedding must be supplied'
         assert not (self.condition_on_text_encodings and (not exists(text_encodings) and not exists(text))), 'text encodings must be present if you specified you wish to condition on it on initialization'
@@ -394,156 +453,7 @@ class BrainDiffusionPriorOld(DiffusionPrior):
         
         return diffusion_prior
     
-class BrainDiffusionPrior(DiffusionPrior):
-    """ 
-    Differences from original:
-    - Allow for passing of generators to torch random functions
-    - Option to include the voxel2clip model and pass voxels into forward method
-    - Return predictions when computing loss
-    - Load pretrained model from @nousr trained on LAION aesthetics
-    """
-    def __init__(self, *args, **kwargs):
-        voxel2clip = kwargs.pop('voxel2clip', None)
-        super().__init__(*args, **kwargs)
-        self.voxel2clip = voxel2clip
 
-    @torch.no_grad()
-    def p_sample_loop_ddpm(self, shape, text_cond, cond_scale = 1., generator=None):
-        batch, device = shape[0], self.device
-        x_start = None 
-
-        # image_embed = x_t(가우시안 노이즈 최종버젼)에서 시작
-        if generator is None:
-            image_embed = torch.randn(shape, device = device)
-        else:
-            image_embed = torch.randn(shape, device = device, generator=generator)
-
-        # image_embedding(x_t) nomalization
-        if self.init_image_embed_l2norm:
-            image_embed = l2norm(image_embed) * self.image_embed_scale
-
-        # t_t -> x_0으로 denoise 시작
-        for i in tqdm(reversed(range(0, self.noise_scheduler.num_timesteps)), desc='sampling loop time step', total=self.noise_scheduler.num_timesteps, disable=True):
-            times = torch.full((batch,), i, device = device, dtype = torch.long)
-
-            self_cond = x_start if self.net.self_cond else None
-            image_embed, x_start = self.p_sample(image_embed, times, text_cond = text_cond, self_cond = self_cond, cond_scale = cond_scale, generator=generator)
-        
-        # image embedding(x_0) nomalization
-        if self.sampling_final_clamp_l2norm and self.predict_x_start:
-            image_embed = self.l2norm_clamp_embed(image_embed)
-
-        return image_embed
-
-    # 한 스텝 denoise 적용
-    @torch.no_grad()
-    def p_sample(self, x, t, text_cond = None, self_cond = None, clip_denoised = True, cond_scale = 1., generator=None):
-        b, *_, device = *x.shape, x.device
-        model_mean, _, model_log_variance, x_start = self.p_mean_variance(x = x, t = t, text_cond = text_cond, self_cond = self_cond, clip_denoised = clip_denoised, cond_scale = cond_scale)
-        
-        # generator를 사용하면 분포에서 고정된 값을 뽑음 -> 매번 같은 이미지 생성
-        if generator is None:
-            noise = torch.randn_like(x)
-        else:
-            noise = torch.randn(x.size(), device=x.device, dtype=x.dtype, generator=generator)
-
-        # t-1에 denoise 
-        # 생성
-        nonzero_mask = (1 - (t == 0).float()).reshape(b, *((1,) * (len(x.shape) - 1))) # x_0이면 noise를 사용하지 않음
-        # 적용
-        pred = model_mean + nonzero_mask * (0.5 * model_log_variance).exp() * noise
-        return pred, x_start
-
-    def p_losses(self, image_embed, times, text_cond, noise = None):
-        # noise 정의
-        noise = default(noise, lambda: torch.randn_like(image_embed)) 
-
-        # random한 t가 들어옴 -> x_0에서 한 번에 t까지의 noise를 씌움 
-        image_embed_noisy = self.noise_scheduler.q_sample(x_start = image_embed, t = times, noise = noise)
-
-        self_cond = None
-        if self.net.self_cond and random.random() < 0.5:
-            with torch.no_grad():
-                self_cond = self.net(image_embed_noisy, times, **text_cond).detach()
-
-        pred = self.net(
-            image_embed_noisy,
-            times,
-            self_cond = self_cond,
-            text_cond_drop_prob = self.text_cond_drop_prob,
-            image_cond_drop_prob = self.image_cond_drop_prob,
-            **text_cond
-        )
-
-        if self.predict_x_start and self.training_clamp_l2norm:
-            pred = self.l2norm_clamp_embed(pred)
-
-        if self.predict_v:
-            target = self.noise_scheduler.calculate_v(image_embed, times, noise)
-        elif self.predict_x_start:
-            target = image_embed
-        else:
-            target = noise
-
-        loss = self.noise_scheduler.loss_fn(pred, target)
-        return loss, pred
-
-    def forward(
-        self,
-        text = None,
-        image = None,
-        voxel = None,
-        text_embed = None,      # allow for training on preprocessed CLIP text and image embeddings
-        image_embed = None,
-        text_encodings = None,  # as well as CLIP text encodings
-        *args,
-        **kwargs
-    ):
-        assert exists(text) ^ exists(text_embed) ^ exists(voxel), 'either text, text embedding, or voxel must be supplied'
-        assert exists(image) ^ exists(image_embed), 'either image or image embedding must be supplied'
-        assert not (self.condition_on_text_encodings and (not exists(text_encodings) and not exists(text))), 'text encodings must be present if you specified you wish to condition on it on initialization'
-
-        if exists(voxel):
-            assert exists(self.voxel2clip), 'voxel2clip must be trained if you wish to pass in voxels'
-            assert not exists(text_embed), 'cannot pass in both text and voxels'
-            if self.voxel2clip.use_projector:
-                clip_voxels_mse, clip_voxels = self.voxel2clip(voxel)
-                text_embed = clip_voxels_mse
-            else:
-                clip_voxels = self.voxel2clip(voxel)
-                text_embed = clip_voxels_mse = clip_voxels
-            # text_embed = self.voxel2clip(voxel)
-
-        if exists(image):
-            image_embed, _ = self.clip.embed_image(image)
-
-        # calculate text conditionings, based on what is passed in
-
-        if exists(text):
-            text_embed, text_encodings = self.clip.embed_text(text)
-
-        text_cond = dict(text_embed = text_embed)
-
-        if self.condition_on_text_encodings:
-            assert exists(text_encodings), 'text encodings must be present for diffusion prior if specified'
-            text_cond = {**text_cond, 'text_encodings': text_encodings}
-
-        # timestep conditioning from ddpm
-
-        batch, device = image_embed.shape[0], image_embed.device
-        times = self.noise_scheduler.sample_random_times(batch)
-
-        # PS: I dont think we need this? also if uncommented this does in-place global variable change
-        # scale image embed (Katherine)
-        # image_embed *= self.image_embed_scale
-
-        # calculate forward loss
-
-        loss, pred = self.p_losses(image_embed*self.image_embed_scale, times, text_cond = text_cond, *args, **kwargs)
-        
-        # undo the scaling so we can directly use it for real mse loss and reconstruction
-        return loss, pred
-    
 from diffusers.models.vae import Decoder
 class Voxel2StableDiffusionModel(torch.nn.Module):
     def __init__(self, in_dim=15724, h=4096, n_blocks=4, use_cont=False, ups_mode='4x'):
